d7cb2438717e9483af163ffc8e49169d3ef39463
Bak Kallemov <kallemov@gmail.com>
2020-06-15 02:39:59 -0700

    latest chages + streamlit

---
+++ asal_streamlit.py
@@ -0,0 +1,103 @@
+import streamlit as st
+import time
+from utils.predict_opt import PredictOptions
+from models import create_model
+from data import  create_predict_data
+from utils.metrics import f1_score_func
+import torch
+import re
+import pandas as pd
+
+@st.cache(allow_output_mutation=True)
+def initialize_model():
+    opt = PredictOptions().parse()   # get training options
+    model = create_model(opt)      # create a model 
+    model.setup(opt)
+    return opt, model
+    
+def get_dataloader(input):
+    dataloader = model.create_dataloader(opt, input, None, randomSample=False)
+    return dataloader
+
+def get_color(attr):
+    # clip values to prevent CSS errors (Values should be from [-1,1])
+    attr = max(-1, min(1, attr))
+    if attr > 0:
+        red   = 255-int(100 * attr)
+        green = 255
+        blue  = 255-int(100 * attr)
+        
+    else:
+        red   = 255
+        green = 255+int(100 * attr)
+        blue  = 255+int(100 * attr)
+    return "#{}{}{}".format(('0x%02x'%red).split('x')[-1], ('0x%02x'%green).split('x')[-1],('0x%02x'%blue).split('x')[-1])
+
+def format_word_importances(words, importances):
+    tags=[]
+    assert len(words) <= len(importances)
+    for word, importance in zip(words, importances[: len(words)]):
+        color = get_color(importance)
+        unwrapped_tag = '\colorbox{{{}}}{{{}}}'.format(color,word.lstrip('#'))
+        tags.append(unwrapped_tag)
+    return "".join(tags)
+
+
+st.title('AutoSAL')
+st.title('Automated Sentiment Analysis Labeling')
+
+opt, model = initialize_model()
+text_type = st.radio('',('single sentence', 'multi-sentence text', 'upload text file'))
+if text_type=='single sentence':
+    text = [st.text_input('Input your sentence here','',max_chars=300)]
+elif text_type=='multi-sentence text':
+    text = re.split('[.\n]',st.text_area('Input your multi-sentence text here (sentence separations are periods \'.\' or Enter',''))
+else:
+    uploaded_file = st.file_uploader("Choose a text file to upload")
+    if uploaded_file is not None:
+        text=uploaded_file.getvalue()
+        st.write(text)
+        text = re.split('[.\n]',text)
+    else:
+        text=''
+        
+text=[x.lstrip(' ') for x in text if len(x.lstrip(' '))>0]    
+
+if len(text)>0:
+    scores, total_score, attributes, vis_data_ig_records = model.predict(get_dataloader(text))
+
+    threshold = st.slider('Threshold for the class probability (confidence score)',0,100,50)
+    
+    scores=[(opt.inv_label_dict[x[0]] if x[1]*100> threshold else 'n/a', x[1]*100, t) for x,t in zip(scores,text)]
+    
+    overall_sentiment = opt.inv_label_dict[total_score.index(max(total_score))]
+    #print(scores, "\t overall sentiment is %s"%overall_sentiment)
+    #print(attributes)
+
+
+    st.subheader('Sentiment for the overall text is: %s'%overall_sentiment)
+    df = pd.DataFrame(
+        scores,
+        columns=['sentiment', 'class probablity (confidence) \%','sentences'])
+    st.write(df)
+    #print(attributes[0][0], attributes[0][1])
+    #visualization.visualize_text(vis_data_ig_records[0])
+
+    if st.checkbox('Show word importance using integrated gradient analysis'):
+
+
+        option = st.selectbox(
+            'For which sentence you like to get word importance? Select index based on the above table.',
+            range(len(text)))#df['sentences'])
+        'You selected:', text[option]
+        
+        ds=format_word_importances(attributes[option][0], attributes[option][1])
+        st.latex('\large'+ds)
+        
+        wi = pd.DataFrame({
+            'tokens': attributes[option][0],
+            'attributes (importance [-1;1])': attributes[option][1]
+        })
+        st.write(wi)
+
+
---
+++ models/captum_bert.py
@@ -0,0 +1,116 @@
+import os
+import torch
+import numpy as np
+from transformers import BertTokenizer
+from transformers import BertForSequenceClassification
+from captum.attr import IntegratedGradients
+from captum.attr import visualization
+
+class BertModelWrapper(torch.nn.Module):
+
+    def __init__(self, opt, model):
+        super(BertModelWrapper, self).__init__()
+        self.opt=opt
+        self.model = model
+        self.device = torch.device('cuda:{}'.format(self.opt.gpu_ids[0])) if self.opt.gpu_ids else torch.device('cpu')  # get device name: CPU or GPU
+        
+    def _compute_bert_outputs(self,model_bert, embedding_output, attention_mask=None, head_mask=None):
+        if attention_mask is None:
+            attention_mask = torch.ones(embedding_output.shape[0], embedding_output.shape[1]).to(embedding_output)
+
+            extended_attention_mask = attention_mask.unsqueeze(1).unsqueeze(2)
+
+            extended_attention_mask = extended_attention_mask.to(dtype=next(model_bert.parameters()).dtype) # fp16 compatibility
+            extended_attention_mask = (1.0 - extended_attention_mask) * -10000.0
+
+            if head_mask is not None:
+                if head_mask.dim() == 1:
+                    head_mask = head_mask.unsqueeze(0).unsqueeze(0).unsqueeze(-1).unsqueeze(-1)
+                    head_mask = head_mask.expand(model_bert.config.num_hidden_layers, -1, -1, -1, -1)
+                elif head_mask.dim() == 2:
+                    head_mask = head_mask.unsqueeze(1).unsqueeze(-1).unsqueeze(-1)  # We can specify head_mask for each layer
+                head_mask = head_mask.to(dtype=next(model_bert.parameters()).dtype) # switch to fload if need + fp16 compatibility
+            else:
+                head_mask = [None] * model_bert.config.num_hidden_layers
+
+            encoder_outputs = model_bert.encoder(embedding_output,
+                                                 extended_attention_mask,
+                                                 head_mask=head_mask)
+            sequence_output = encoder_outputs[0]
+            sequence_output.to(self.device)
+            pooled_output = model_bert.pooler(sequence_output)
+            pooled_output.to(self.device)
+            outputs = (sequence_output, pooled_output,) + encoder_outputs[1:]  # add hidden_states and attentions if they are here
+            return outputs  # sequence_output, pooled_output, (hidden_states), (attentions)
+
+    def forward(self, embeddings):        
+        outputs = self._compute_bert_outputs(self.model.bert, embeddings)
+        
+        pooled_output = outputs[1]
+        #pooled_output = self.model.dropout(pooled_output)
+        logits = self.model.classifier(pooled_output)
+        return torch.softmax(logits, dim=1)[:, 1].unsqueeze(1)
+
+class captum_bert():
+
+    def __init__(self, opt):
+        self.opt=opt
+        self.vis_data_records_ig = []
+        self.isDefined = False
+            
+    def show_words_importance(self):
+        visualization.visualize_text(self.vis_data_records_ig)
+        
+    def setup(self, model):#setup a captum wrapper model based on bert
+        self.device = torch.device('cuda:{}'.format(self.opt.gpu_ids[0])) if self.opt.gpu_ids else torch.device('cpu')  # get device name: CPU or GPU
+        self.bert_model_wrapper = BertModelWrapper(self.opt, model)
+        self.bert_model_wrapper.to(self.device)
+        self.ig = IntegratedGradients(self.bert_model_wrapper)
+        self.isDefined=True
+    
+
+    def interpret_sentence(self, tokens, input_embedding, label=0):
+
+        if not self.isDefined:
+            print('word interpretation model is not setup up!')
+            exit(1)
+        
+        self.vis_data_records_ig = []
+            
+        # compute attributions and approximation delta using integrated gradients
+        attributions, delta = self.ig.attribute(input_embedding, n_steps=self.opt.num_captum_iterations, return_convergence_delta=True)
+        
+        pred,pred_ind=0.,0
+        
+        #tokens = tokenizer.convert_ids_to_tokens(input_ids[0].detach().cpu().numpy().tolist())    
+        attributions = attributions.sum(dim=2).squeeze(0)
+        attributions = attributions / torch.norm(attributions)
+        attributions = attributions.detach().cpu().numpy()
+        attributions[0]=0
+        attributions[-1]=0
+        attributions /=max(attributions)
+        #print(attributions)
+        if self.opt.captum_visualization:
+            #add_attributions_to_visualizer(attributions_ig, tokens, pred, pred_ind, label, delta, vis_data_records_ig)
+            self.add_attributions_to_visualizer(attributions, tokens, 
+                                                  pred, pred_ind, label, delta, 
+                                                  self.vis_data_records_ig)
+        return attributions 
+            
+                
+    def add_attributions_to_visualizer(self, attributions, tokens, pred, pred_ind, label, delta, vis_data_records):
+        # storing couple samples in an array for visualization purposes
+        vis_data_records.append(visualization.VisualizationDataRecord(
+            attributions[1:-1],
+            pred,
+            pred_ind,
+            label,
+            "label",
+            attributions.sum(),       
+            tokens[1:-1],
+            delta
+        ))    
+
+            
+
+
